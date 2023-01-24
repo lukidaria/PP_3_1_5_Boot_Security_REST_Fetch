db375226c803c9af89464b3ee29823f5c7d8f6fb
Luki Daria <one_snowly@mail.ru>
2023-01-25 02:39:37 +0300

    added hascode & equals methods

--- src/main/java/ru/luki/pp_315_boot_sec_rest_fetch/model/User.java
+++ src/main/java/ru/luki/pp_315_boot_sec_rest_fetch/model/User.java
@@ -146,10 +146,6 @@ public class User implements UserDetails {
         return username;
     }
 
-
-
-
-
     @Override
     public boolean isAccountNonExpired() {
         return true;
@@ -167,6 +163,38 @@ public class User implements UserDetails {
 
     @Override
     public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof User)) {
+            return false;
+        }
+        User other = (User) obj;
+        if (username == null) {
+            if (other.username != null) {
+                return false;
+            }
+        } else if (!username.equals(other.username)) {
+            return false;
+        }
+
         return true;
     }
 }
